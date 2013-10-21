6c37337c4367372f0bc8ae09f5ca4cef4f65793b
florakarniav <flora91k@windowslive.com>
2013-10-21 19:34:35 +0300

    Initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "florakarniav"
+client_key               "#{current_dir}/florakarniav.pem"
+validation_client_name   "florakarniav-validator"
+validation_key           "#{current_dir}/florakarniav-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/florakarniav"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
